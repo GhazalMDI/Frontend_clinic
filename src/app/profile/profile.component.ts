629aef011ac5d65fa0b4f39ba256a214ffca9ffd
Ghazalmdi <ghazal.mohammadi.telecom@gmail.com>
2025-02-08 12:18:49 +0330

    complated work_hours form in Html/Ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ProfileService } from '../Service/profile.service';
 import { response } from 'express';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { error } from 'console';
 import { Router } from '@angular/router';
 
@@ -15,10 +15,24 @@ export class ProfileComponent {
   information: any[] = []
   DoctorprofileForm!: FormGroup
   ProfileForm!: FormGroup
+  WorkingHoursForm!:FormGroup
   is_doctor: Boolean = true;
   image!: String;
   toastMessage!: { message: string; type: 'success' | 'error' };
   isToastVisible!: boolean;
+  workSchedule !: []
+
+  DAYS_MAP: { [key: string]: string } = {
+    "5": "شنبه",
+    "6": "یکشنبه",
+    "0": "دوشنبه",
+    "1": "سه‌شنبه",
+    "2": "چهارشنبه",
+    "3": "پنجشنبه",
+    "4": "جمعه"
+  };
+
+
 
 
   constructor(private fb: FormBuilder, private prof: ProfileService, private router: Router) { }
@@ -32,8 +46,13 @@ export class ProfileComponent {
       bio: [''],
       landline_phone: [''],
       medical_license_number: ['']
-
     })
+    this.WorkingHoursForm = this.fb.group({
+      workSchedule: this.fb.array([]) // آرایه‌ای برای روزهای کاری
+    });
+  
+
+
     this.ProfileForm = this.fb.group({
       first_name: [''],
       last_name: [''],
@@ -44,10 +63,20 @@ export class ProfileComponent {
   }
 
   showProfile() {
+
+
+   
     this.prof.GetProfileData().subscribe(
+
+      
+
+
       (response) => {
         console.log(response.data)
+        console.log(response.data.work_hours)
         const userData = response.data.user;
+        const work_hours = response.data.work_hours;
+        console.log(work_hours)
         if (response.data.status_doctor == true) {
           this.is_doctor = true
           this.DoctorprofileForm.patchValue({
@@ -60,7 +89,26 @@ export class ProfileComponent {
             medical_license_number: userData.medical_license_number
           })
           this.image = userData.image
+
+          if (work_hours.length > 0) {
+            const workScheduleArray = this.WorkingHoursForm.get('workSchedule') as FormArray;
+            // workScheduleArray.clear(); // پاک کردن داده‌های قبلی
+      
+            work_hours.forEach((wh:any) => {
+              workScheduleArray.push(this.fb.group({
+                day: new FormControl(this.DAYS_MAP[wh.day] || "نامشخص"),
+                start_time: new FormControl(wh.start_time),
+                end_time: new FormControl(wh.end_time)
+              }));
+            });
+
+          // this.WorkingHoursForm.patchValue({
+          //   day: dayText,
+          //   start_time:workSchedule.start_time,
+          //   end_time:workSchedule.end_time
+          // })
         }
+      }
         else if (response.data.status_doctor == false) {
           console.log('the user')
           console.log(response.data.status_doctor)
@@ -75,12 +123,15 @@ export class ProfileComponent {
 
         }
         this.information = this.information = Array.isArray(response.data) ? response.data : [response.data];
-
       },
       (error) => {
         console.error('Error occurred:', error);
       }
     )
+  }
+
+  get workScheduleArray(): FormArray {
+    return this.WorkingHoursForm.get('workSchedule') as FormArray;
   }
 
   editProfile() {
